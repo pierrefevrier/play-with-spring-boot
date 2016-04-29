d4dd6ecad8409419696c8901a2cfe0f27d0f5173
Pierre FEVRIER <git@pierrefevrier.fr>
2016-04-30 01:31:16 +0200

    Show command line args mapping

--- dev/hello-world-sb-jersey-command-line-args/run.bat
+++ dev/hello-world-sb-jersey-command-line-args/run.bat
@@ -1,4 +1,4 @@
 call mvn clean package spring-boot:run ^
-	-D run.arguments="--start" ^
+	-D run.arguments="--start,--parallelism=5" ^
 	-f %WORKSPACE_HOME%/hello-world-sb-jersey-command-line-args/pom.xml
 pause
